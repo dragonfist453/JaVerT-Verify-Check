679cb647f4cf3dafb327f61147ae8410c8911c8b
Karthik Ambu <thermosphere453@gmail.com>
2024-04-10 04:53:07 +0100

    Fixed pos, implement verification run

--- src/verifyDebugger.ts
+++ src/verifyDebugger.ts
@@ -1,5 +1,6 @@
 import * as vscode from 'vscode';
 import * as path from 'path';
+import * as fs from 'fs';
 
 export class VerifierDebugger {
     private _decorationType: vscode.TextEditorDecorationType | undefined;
@@ -23,15 +24,16 @@ export class VerifierDebugger {
         const doc = editor.document;
         const text = doc.getText();
 
-        // Regular expression to find @id annotations in JavaScript comments
-        const idPattern = /\/\*\*[\s\S]*?@id\s*(\w+)?[\s\S]*?\*\/|\/\/\s*@id\s*(\w+)?/gm;
+        // Regular expression to find @id annotations in JavaScript docstrings for functions
+        const idPattern = /(\/\*\*[\s\S]*?@id\s*(\w+)?[\s\S]*?\*\/|\/\/\s*@id\s*(\w+)?)([\s\S]*?function\s+([a-zA-Z_$][0-9a-zA-Z_$]*)\s*\([^)]*\)\s*\{[^}]*\})/gm;
         let matches;
         let decorations: vscode.DecorationOptions[] = [];
 
         while ((matches = idPattern.exec(text)) !== null) {
-            const id = matches[1] || matches[2]; // Capturing group 1 or 2 will contain the @id value
+            const id = matches[4]; // Capturing group 1 or 2 will contain the @id value
             if (id) {
-                const line = text.substring(matches.index).split('\n').length - 1;
+                const funcLine = id.split('\n').find((l) => l.includes('function'));
+                const line = text.split('\n').findIndex((l) => l.includes(funcLine || ''));
                 const startPos = new vscode.Position(line, 0);
                 const endPos = new vscode.Position(line, text.split('\n')[line].length);
                 const range = new vscode.Range(startPos, endPos);
@@ -46,6 +48,29 @@ export class VerifierDebugger {
     }
 
     verificationPassed() {
+        if (!vscode.window.activeTextEditor) {
+            return;
+        }
+
+        const terminal = vscode.window.terminals.find((t) => t.name === 'Javert')|| vscode.window.createTerminal('Javert');
+
+        const filePath = vscode.window.activeTextEditor?.document.uri.fsPath;
+        const dirPath = filePath ? path.dirname(filePath) : '';
+        const fileName = filePath ? path.basename(filePath) : '';
+        if (!filePath || !fs.existsSync(filePath) || !fs.existsSync(dirPath) || !fileName.endsWith('.js')) {
+            vscode.window.showErrorMessage('No file path found.');
+            return;
+        }
+
+        const outPath = path.join(dirPath, '.gillian');
+        if (!fs.existsSync(outPath)) {
+            fs.mkdirSync(outPath);
+        }
+
+        terminal.sendText(
+            `docker run -ti --rm -v ${dirPath}:/app/test-algorithms 677877e05b9b gillian-js verify /app/test-algorithms/${fileName} --result-dir=/app/test-algorithms/.gillian`,
+            true
+        );
         vscode.window.showInformationMessage('Verification complete.');
     }
 
